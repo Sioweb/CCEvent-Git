7a489a0a1c91a67e6a9696668a50d9bac40bd30f
Sascha Weidner <s.weidner@seipp.com>
2020-03-24 08:36:44 +0100

    Init

---
+++ src/Command/AddGitCommand.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace Sioweb\CCEvent\Git\Command;
+
+use Symfony\Bundle\FrameworkBundle\Command\ContainerAwareCommand;
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputOption;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Console\Style\SymfonyStyle;
+use Symfony\Component\Filesystem\Filesystem;
+use Symfony\Component\Finder\Finder;
+use Symfony\Component\Finder\SplFileInfo;
+/**
+ * My own command
+ *
+ * Demo command for learning
+ */
+class AddGitCommand extends ContainerAwareCommand
+{
+    /**
+     * @var SymfonyStyle
+     */
+    private $io;
+
+    /**
+     * @var array
+     */
+    private $rows = [];
+
+    /**
+     * @var string
+     */
+    private $rootDir;
+
+    /**
+     * @var string
+     */
+    private $webDir;
+
+    /**
+     * @var int
+     */
+    private $statusCode = 0;
+
+    /**
+     * {@inheritdoc}
+     */
+    protected function configure(): void
+    {
+        $this
+            ->setName('ccevent:add:git')
+            ->setDefinition([
+                new InputArgument('repository', InputArgument::REQUIRED, 'The url to the git repository'),
+                new InputArgument('package', InputArgument::REQUIRED, 'The package name'),
+                new InputOption('target-dir', 'td', InputOption::VALUE_OPTIONAL, 'The composer target dir'),
+                new InputOption('vendor-dir', 'vd', InputOption::VALUE_OPTIONAL, 'The composer vendor dir'),
+            ])
+            ->setDescription('Initialize the git repository, in the package for better development in vendor (Only recomended on localhost!).')
+        ;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    protected function execute(InputInterface $input, OutputInterface $output): int
+    {
+        echo "Loading git repositories.\n";
+        $this->io = new SymfonyStyle($input, $output);
+        $this->rootDir = $this->getContainer()->getParameter('kernel.project_dir');
+        $this->gitUrl = rtrim($input->getArgument('repository'), '/');
+        $this->packageName = rtrim($input->getArgument('package'), '/');
+        exec("cd \"".$this->rootDir.'/vendor/'.$this->packageName."\" && git init && git remote add origin ".$this->gitUrl." && git fetch --all && git reset --hard origin/master 2>&1", $output);
+        // print_r($output);
+        // die();
+
+        // if (!empty($this->rows)) {
+        //     $this->io->newLine();
+        //     $this->io->table(['', 'GIT', 'Url / Error'], $this->rows);
+        // }
+
+        return $this->statusCode;
+    }
+}
---
+++ src/Composer/Git.php
@@ -0,0 +1,133 @@
+<?php
+
+namespace Sioweb\CCEvent\Git\Composer;
+
+use Composer\Package\Dumper\ArrayDumper;
+use Sioweb\CCEvent\Composer\Installer\PackageEvent as Event;
+use Symfony\Component\Console\Input\InputDefinition;
+use Symfony\Component\Console\Input\InputOption;
+use Symfony\Component\Console\Input\StringInput;
+use Symfony\Component\Process\PhpExecutableFinder;
+use Symfony\Component\Process\Process;
+
+class Git
+{
+
+    public static function getInitDefinition()
+    {
+        $InputDefinition = new InputDefinition;
+        $InputDefinition->setDefinition([
+            new InputOption('repository', 'r', InputOption::VALUE_REQUIRED, 'url to git repo')
+        ]);
+
+        return $InputDefinition;
+    }
+
+    public static function init(Event $event): void
+    {
+        $operation = $event->getOperation();
+        
+        $package = method_exists($event->getOperation(), 'getPackage')
+            ? $operation->getPackage()
+            : $operation->getInitialPackage();
+
+        $Input = new StringInput(implode(' ', $event->getArguments()));
+        $Input->bind(self::getInitDefinition());
+        $Arguments = $Input->getOptions();
+
+        // die('<pre>' . __METHOD__ . ":\n" . print_r([
+        //     $Arguments['repository'],
+        //     $package->getName(),
+        //     '--target-dir='.$package->getSourceUrl(),
+        //     '--vendor-dir='.$event->getComposer()->getConfig()->get('vendor-dir'),
+        // ], true) . "\n#################################\n\n" . '</pre>');
+        
+        static::executeCommand('ccevent:add:git ' . implode(' ', [
+            $Arguments['repository'],
+            $package->getName(),
+            '--target-dir='.$package->getSourceUrl(),
+            '--vendor-dir='.$event->getComposer()->getConfig()->get('vendor-dir'),
+        ]), $event);
+    }
+
+    private static function getWebDir(Event $event): string
+    {
+        $extra = $event->getComposer()->getPackage()->getExtra();
+
+        return $extra['symfony-web-dir'] ?? 'web';
+    }
+
+    /**
+     * @throws \RuntimeException
+     */
+    private static function executeCommand(string $cmd, Event $event): void
+    {
+        $phpFinder = new PhpExecutableFinder();
+
+        if (false === ($phpPath = $phpFinder->find())) {
+            throw new \RuntimeException('The php executable could not be found.');
+        }
+
+        $binPath = $event->getComposer()->getConfig()->get('vendor-dir').'/bin/';
+        if(file_exists($binPath . 'console')) {
+            $execFile = $binPath . 'console';
+        } elseif(file_exists($binPath . 'contao-console')) {
+            $execFile = $binPath . 'contao-console';
+        } elseif(file_exists($binPath . 'oxid')) {
+            $execFile = $binPath . 'oxid';
+        }
+
+        $process = new Process(
+            sprintf(
+                '%s %s%s %s%s --env=%s',
+                $phpPath,
+                $execFile,
+                $event->getIO()->isDecorated() ? ' --ansi' : '',
+                $cmd,
+                self::getVerbosityFlag($event),
+                getenv('SYMFONY_ENV') ?: 'prod'
+            )
+        );
+
+        $process->run(
+            function (string $type, string $buffer) use ($event): void {
+                $event->getIO()->write($buffer, false);
+            }
+        );
+
+        if (!$process->isSuccessful()) {
+            throw new \RuntimeException(sprintf('An error occurred while executing the "%s" command.', $cmd));
+        }
+    }
+
+    private static function getBinDir(Event $event): string
+    {
+        $extra = $event->getComposer()->getPackage()->getExtra();
+
+        // Symfony assumes the new directory structure if symfony-var-dir is set
+        if (isset($extra['symfony-var-dir']) && is_dir($extra['symfony-var-dir'])) {
+            return $extra['symfony-bin-dir'] ?? 'bin';
+        }
+
+        return $extra['symfony-app-dir'] ?? 'app';
+    }
+
+    private static function getVerbosityFlag(Event $event): string
+    {
+        $io = $event->getIO();
+
+        switch (true) {
+            case $io->isDebug():
+                return ' -vvv';
+
+            case $io->isVeryVerbose():
+                return ' -vv';
+
+            case $io->isVerbose():
+                return ' -v';
+
+            default:
+                return '';
+        }
+    }
+}
